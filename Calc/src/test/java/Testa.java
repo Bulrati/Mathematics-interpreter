c2d3b81d71af1cf08a79bd484437b96db2094d43
Gesser <artxxv@gmail.com>
2013-11-10 13:52:58 +0200

    v.1.1
    more tests

--- Calc/src/test/java/Testa.java
+++ Calc/src/test/java/Testa.java
@@ -5,9 +5,29 @@ import static org.junit.Assert.assertEquals;
 
 public class Testa {
     @Test
-    public void testAdd() {
+    public void testPlus() {
     String example = "2+2";
         assertEquals(Instance.eval(example), 4.0, 0.001);
+    }
+    @Test
+    public void testMinus() {
+        String example = "7-2";
+        assertEquals(Instance.eval(example), 5.0, 0.001);
+    }
+    @Test
+    public void testMultiply() {
+        String example = "3*3";
+        assertEquals(Instance.eval(example), 9.0, 0.001);
+    }
+    @Test
+    public void testDivide() {
+        String example = "8/2";
+        assertEquals(Instance.eval(example), 4.0, 0.001);
+    }
+    @Test
+    public void testPower() {
+        String example = "4^2";
+        assertEquals(Instance.eval(example), 16.0, 0.001);
     }
 
        @Test
@@ -21,5 +41,7 @@ public class Testa {
                 String example = "(-2+(5-1))";
                 assertEquals(Instance.eval(example), 2.0, 0.001);
             }
+
+
 
 }
